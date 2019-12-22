8b4e89475214c0fc55b58f4929574e67cca045b2
DMovsumov <movsumov.denis@gmail.com>
2019-12-22 21:48:21 +0500

    Доработан header

---
+++ js/translate.js
@@ -0,0 +1,22 @@
+const lang_english = ['All goals', 'Reach the goal', 'On the way to achieving', 'Create new goal']//Записываем в массив перевод всех слов в навигационном меню
+const lang = document.querySelectorAll('.lang')//Ко всем словам меню добавляем модификатор lang
+const headerRange = document.querySelector('#header-range')//Получаем range по id
+
+
+//Создаем переменную и с помощью цикла записываем в нее все слова меню на странице
+let lang_russian = []
+for(let i = 0; i < lang.length; i++){
+    lang_russian[i] = lang[i].innerHTML
+}
+
+
+headerRange.addEventListener('change', (e) => {
+    for(let i = 0; i < lang.length; i++){
+        if(e.target.step == 0){
+            lang[i].innerHTML = lang_english[i]
+        }
+        if(e.target.step == 1) {
+            lang[i].innerHTML = lang_russian[i]
+        }
+    }
+})
